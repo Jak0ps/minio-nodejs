bf6bd47963621b0b7ae9dda5df174a0a950d2692
Jacob Saleh <jacobsaleh@gmail.com>
2019-03-01 18:18:39 +0200

    Updates

---
+++ app/creds.js
@@ -0,0 +1,13 @@
+module.exports = {
+    accessKey :"foobar",
+    secretKey :"blabla"
+}
+//
+//module.exports = {
+//    accessKey: function() {
+//        return "foobar"
+//    },
+//    secretKey: function() {
+//        return "blabla"
+//    }
+//}
