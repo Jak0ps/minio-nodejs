ea22774ec5c9c90ad167a7c2c48b26320150edbb
Jacob Saleh <jacobsaleh@gmail.com>
2019-03-02 19:16:38 +0200

    Update server.js

--- app/server.js
+++ app/server.js
@@ -30,9 +30,18 @@ server.get('/presignedUrl', (req, res) => {
     })
 })
 
+server.get('/statusCheck', (req, res) => {
+    res.sendStatus(200);
+})
+
+//server.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+//})
+
 server.get('/', (req, res) => {
-    //res.sendFile(__dirname + '/index.html');
-    res.sendFile('http://minio1:9000/miniofe/index.html');
+    res.statusCode = 200;
+    res.setHeader("Location", "http://minio1:9000/miniofe/index.html");
+    res.end();
 })
 
 server.listen(8088)
